b753fb56c524fbf33449a2a65025253a011bc7f6
Zian Chen <ancenxixi@gmail.com>
2017-02-26 23:03:02 -0800

    Create QueueByTwoStack.java

---
+++ code/com/laioffer/anson/QueueByTwoStack.java
@@ -0,0 +1,42 @@
+public class QueueByTwoStack {
+	private Deque<Integer> pushStack;
+	private Deque<Integer> popStack;
+	
+	public Solution() {
+		pushStack = new ArrayDeque<Integer>();
+		popStack = new ArrayDeque<Integer>();
+	}
+
+	public Integer poll() {
+		if (popStack.isEmpty()) {
+			while (!pushStack.isEmpty()) {
+				popStack.offer(pushStack.poll());
+			}
+		} 
+		return popStack.poll();
+	}
+
+	public void offer(int element) {
+		pushStack.offer(element);
+	}
+
+	public Integer peek() {
+		if (popStack.isEmpty()) {
+			while (!pushStack.isEmpty()) {
+				popStack.offer(pushStack.poll());
+			}
+		} 
+		return popStack.peek();
+	}
+
+	public int size() {
+		if (popStack.isEmpty() && pushStack.isEmpty()) {
+			return 0;
+		} 
+		return popStack.size() + pushStack.size();
+	}
+
+	public boolean isEmpty() {
+		return popStack.isEmpty() && pushStack.isEmpty();
+	}
+}
